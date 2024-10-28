06e1ec228f1e7127991c9468346a71ffafdbb440
jessica needham <jessiefneedham@gmail.com>
2024-10-28 14:16:11 -0700

    updates to climate and co2 forcings

--- global_e3sm_fates-cnp.sh
+++ global_e3sm_fates-cnp.sh
@@ -42,23 +42,23 @@ cd $CASE_NAME
 if [ "$STAGE" = "AD_SPINUP"  ]; then
 
     ./xmlchange RUN_STARTDATE=0001-01-01
-    ./xmlchange RESUBMIT=1
-    ./xmlchange STOP_N=30
+    ./xmlchange RESUBMIT=7
+    ./xmlchange STOP_N=25
     ./xmlchange REST_N=5
     ./xmlchange STOP_OPTION=nyears
     ./xmlchange REST_OPTION=nyears
     ./xmlchange SAVE_TIMING=FALSE
     ./xmlchange JOB_QUEUE=regular
-    ./xmlchange JOB_WALLCLOCK_TIME=06:00:00
+    ./xmlchange JOB_WALLCLOCK_TIME=08:00:00
     ./xmlchange CCSM_CO2_PPMV=287.
 
     ./xmlchange ELM_ACCELERATED_SPINUP=on
     ./xmlchange ELM_BLDNML_OPTS="-bgc fates -no-megan -no-drydep -nutrient cnp -nutrient_comp_pathway rd -soil_decomp century -bgc_spinup on"
     
     ./xmlchange DATM_MODE=CLMGSWP3v1
-    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1965
-    ./xmlchange DATM_CLMNCEP_YR_START=1965
-    ./xmlchange DATM_CLMNCEP_YR_END=2013
+    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1900
+    ./xmlchange DATM_CLMNCEP_YR_START=1900
+    ./xmlchange DATM_CLMNCEP_YR_END=1925
     # Read climate data from DVS (on CFS) in read-only mode - magic speed gains
     ./xmlchange DIN_LOC_ROOT="/dvs_ro/cfs/cdirs/e3sm/inputdata"
     ./xmlchange DIN_LOC_ROOT_CLMFORC="/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7"
@@ -90,27 +90,33 @@ use_century_decomp = .true.
 spinup_state = 1
 suplphos = 'ALL'
 suplnitro = 'ALL'
+hist_fincl1=
+'FATES_VEGC', 'FATES_FRACTION', 'FATES_GPP','FATES_NEP','FATES_AUTORESP', 'FATES_HET_RESP', 'QVEGE', 'QVEGT',
+'QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF',
+'FATES_GPP_PF', 'FATES_NPP_PF', 'FATES_CROWNAREA_CANOPY_SZ', 'FATES_CROWNAREA_USTORY_SZ', 'FATES_VEGC_SZ', 
+'FATES_NOCOMP_NPATCHES_PF','FATES_NOCOMP_PATCHAREA_PF', 'FATES_MORTALITY_CSTARV_SZ', 'FATES_NPLANT_SZ',
+'FATES_NPLANT_PF'
 EOF
 
 elif [ "$STAGE" = "POSTAD_SPINUP" ]; then
 
-    ./xmlchange RUN_STARTDATE=0001-01-01
-    ./xmlchange RESUBMIT=0
-    ./xmlchange STOP_N=1
-    ./xmlchange REST_N=1
+    ./xmlchange RUN_STARTDATE=0200-01-01
+    ./xmlchange RESUBMIT=23
+    ./xmlchange STOP_N=25
+    ./xmlchange REST_N=5
     ./xmlchange STOP_OPTION=nyears
     ./xmlchange REST_OPTION=nyears
     ./xmlchange JOB_QUEUE=regular
-    ./xmlchange JOB_WALLCLOCK_TIME=06:00:00
+    ./xmlchange JOB_WALLCLOCK_TIME=08:00:00
     ./xmlchange CCSM_CO2_PPMV=287.
 
     ./xmlchange ELM_ACCELERATED_SPINUP=off
     ./xmlchange ELM_BLDNML_OPTS="-bgc fates -no-megan -no-drydep -nutrient cnp -nutrient_comp_pathway rd -soil_decomp century"
     
     ./xmlchange DATM_MODE=CLMGSWP3v1
-    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1965
-    ./xmlchange DATM_CLMNCEP_YR_START=1965
-    ./xmlchange DATM_CLMNCEP_YR_END=2013
+    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1900
+    ./xmlchange DATM_CLMNCEP_YR_START=1900
+    ./xmlchange DATM_CLMNCEP_YR_END=1925
     # Read climate data from DVS (on CFS) in read-only mode - magic speed gains
     ./xmlchange DIN_LOC_ROOT="/dvs_ro/cfs/cdirs/e3sm/inputdata"
     ./xmlchange DIN_LOC_ROOT_CLMFORC="/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7"
@@ -143,27 +149,36 @@ use_century_decomp = .true.
 spinup_state = 0
 suplphos = 'ALL'
 suplnitro = 'NONE'
+hist_fincl1=
+'FATES_VEGC', 'FATES_FRACTION', 'FATES_GPP','FATES_NEP','FATES_AUTORESP', 'FATES_HET_RESP', 'QVEGE', 'QVEGT',
+'QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF',
+'FATES_GPP_PF', 'FATES_NPP_PF', 'FATES_CROWNAREA_CANOPY_SZ', 'FATES_CROWNAREA_USTORY_SZ', 'FATES_VEGC_SZ', 
+'FATES_NOCOMP_NPATCHES_PF','FATES_NOCOMP_PATCHAREA_PF', 'FATES_MORTALITY_CSTARV_SZ', 'FATES_NPLANT_SZ',
+'FATES_NPLANT_PF'
 EOF
 
 elif [ "$STAGE" = "TRANSIENT" ]; then
 
     ./xmlchange RUN_STARTDATE=2000-01-01
-    ./xmlchange RESUBMIT=0
-    ./xmlchange STOP_N=1
-    ./xmlchange REST_N=1
+    ./xmlchange RESUBMIT=6
+    ./xmlchange STOP_N=25
+    ./xmlchange REST_N=5
     ./xmlchange STOP_OPTION=nyears
     ./xmlchange REST_OPTION=nyears
     ./xmlchange JOB_QUEUE=regular
-    ./xmlchange JOB_WALLCLOCK_TIME=06:00:00
-    ./xmlchange CCSM_CO2_PPMV=287.
-
+    ./xmlchange JOB_WALLCLOCK_TIME=08:00:00
     ./xmlchange ELM_ACCELERATED_SPINUP=off
     ./xmlchange ELM_BLDNML_OPTS="-bgc fates -no-megan -no-drydep -nutrient cnp -nutrient_comp_pathway rd -soil_decomp century"
     
+    # NOW WE WANT TIMESERIES OF CO2 AND CLIMATE
+    ./xmlchange CCSM_BGC=CO2A
+    ./xmlchange ELM_CO2_TYPE=diagnostic
+    ./xmlchange DATM_CO2_TSERIES=20tr
+
     ./xmlchange DATM_MODE=CLMGSWP3v1
-    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1965
-    ./xmlchange DATM_CLMNCEP_YR_START=1965
-    ./xmlchange DATM_CLMNCEP_YR_END=2013
+    ./xmlchange DATM_CLMNCEP_YR_ALIGN=1925
+    ./xmlchange DATM_CLMNCEP_YR_START=1925
+    ./xmlchange DATM_CLMNCEP_YR_END=2014
     # Read climate data from DVS (on CFS) in read-only mode - magic speed gains
     ./xmlchange DIN_LOC_ROOT="/dvs_ro/cfs/cdirs/e3sm/inputdata"
     ./xmlchange DIN_LOC_ROOT_CLMFORC="/dvs_ro/cfs/cdirs/e3sm/inputdata/atm/datm7"
@@ -196,6 +211,12 @@ use_century_decomp = .true.
 spinup_state = 0
 suplphos = 'ALL'
 suplnitro = 'NONE'
+hist_fincl1=
+'FATES_VEGC', 'FATES_FRACTION', 'FATES_GPP','FATES_NEP','FATES_AUTORESP', 'FATES_HET_RESP', 'QVEGE', 'QVEGT',
+'QSOIL','EFLX_LH_TOT','FSH','FSR', 'FSDS','FSA','FIRE','FLDS','FATES_LAI', 'FATES_VEGC_PF',
+'FATES_GPP_PF', 'FATES_NPP_PF', 'FATES_CROWNAREA_CANOPY_SZ', 'FATES_CROWNAREA_USTORY_SZ', 'FATES_VEGC_SZ', 
+'FATES_NOCOMP_NPATCHES_PF','FATES_NOCOMP_PATCHAREA_PF', 'FATES_MORTALITY_CSTARV_SZ', 'FATES_NPLANT_SZ',
+'FATES_NPLANT_PF'
 EOF
     
 fi
